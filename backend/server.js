45240d3598078815e1d1154d715ce7b53efe9f35
boonboww <anhquan5504@gmail.com>
2025-04-21 23:10:45 +0700

    #4

--- backend/server.js
+++ backend/server.js
@@ -3,31 +3,39 @@ import cookieParser from 'cookie-parser';
 
 // import route
 import authRoutes from './routes/auth.route.js';
+import ticketRoutes from './routes/ticket.route.js';
 import accountManagerRoutes from './routes/accountManager.routes.js'
 
 import { connectDB } from './config/bd.js';
 import { ENV_VARS } from './config/envVars.js';
 
-
 const app = express();
 const PORT = ENV_VARS.PORT;
-// tao bien moi truong
+
 
 app.use(cookieParser());
-app.use(express.json()); 
+app.use(express.json());
 
 
+app.use('/api/v1/ticket', ticketRoutes);
+app.use('/api/v1/auth', authRoutes);
+console.log('MONGO_URI: ', process.env.MONGO_URI);
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database', err);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/account", accountManagerRoutes); 
 console.log("MONGO_URI: ", process.env.MONGO_URI);
-
+  })
 
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}).catch(err => {
-  console.error("Failed to connect to the database", err);
-});
-
+  });});
